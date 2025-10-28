a9d8c09f2fdc7723a925bf214a5a555c882c3cbd
Aleksey Kurylev <alekseykurylev@yandex.ru>
2025-10-28 13:43:57 +0300

    style: note

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,14 +1,29 @@
 import type { ComponentProps } from "react";
-import { cx } from "../../lib/cva.ts";
+import { cva, type VariantProps } from "cva";
 
-export function Button({ className, ...props }: ComponentProps<"button">) {
+const button = cva({
+  base: "rounded-full border border-neutral-600 bg-neutral-700/40 p-2 hover:not-disabled:bg-neutral-700/80",
+  variants: {
+    disabled: {
+      false: null,
+      true: "disabled:bg-neutral-700/20 disabled:text-white/30",
+    },
+  },
+  defaultVariants: {
+    disabled: false,
+  },
+});
+
+export function Button({
+  className,
+  disabled,
+  ...props
+}: ComponentProps<"button"> & VariantProps<typeof button>) {
   return (
     <button
       type="button"
-      className={cx(
-        "rounded-full border border-neutral-600 bg-neutral-700/40 p-2 hover:not-disabled:bg-neutral-700/80 disabled:text-white/50",
-        className,
-      )}
+      className={button({ disabled, className })}
+      disabled={disabled || undefined}
       {...props}
     />
   );
